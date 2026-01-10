e2e5ad1859af780e299968da7a489e22c3bc3e83
carlosninacio <cigramirez@poligran.edu.co>
2026-01-10 09:28:10 -0500

    petición get en front para obtener productos

--- frontend/inventario-app/src/app/producto.service.ts
+++ frontend/inventario-app/src/app/producto.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Producto } from './producto';
 
 @Injectable({
   providedIn: 'root',
@@ -6,5 +9,11 @@ import { Injectable } from '@angular/core';
 export class ProductoService {
   
   private urlBase = "http://localhost:8080/inventario-app/productos";
+  private clienteHttp = inject(HttpClient);
 
+  obtenerProductosLista(): Observable<Producto[]> {
+    return this.clienteHttp.get<Producto[]>(this.urlBase);
+  }
+
+  
 }
