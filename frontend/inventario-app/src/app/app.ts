bfe598997391b8e8c1b8c6f450bd9c971ad7e8a4
carlosninacio <cigramirez@poligran.edu.co>
2026-01-10 09:11:17 -0500

    Uso de ruta en index y tabla ejemplo de bootstrap

--- frontend/inventario-app/src/app/app.ts
+++ frontend/inventario-app/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductoLista } from "./producto-lista/producto-lista";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [ProductoLista]
 })
 export class App {
   protected readonly title = signal('inventario-app');
